6f8d7e404b6536dc13c6a91da0cc3b978a2f18c1
Niklas <NiklasAkita@users.noreply.github.com>
2023-09-20 08:49:29 +0200

    added hold modifiers

--- keyboards/splitkb/aurora/lily58/keymaps/custom/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/custom/keymap.c
@@ -32,7 +32,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
@@ -59,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_COLEMAK_DH] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_MINS,
-  KC_LCTL,  KC_A,   KC_R,    KC_S,    KC_T,    KC_G,                     KC_M,    KC_N,    KC_E,    KC_I,    KC_O, KC_QUOT,
+  KC_LCTL,  KC_A , LALT_T(KC_R), CTL_T(KC_S), SFT_T(KC_T), KC_G,         KC_M,    RSFT_T(KC_N),    RCTL_T(KC_E),   RALT_T(KC_I),    KC_O, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_LBRC,  KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                         KC_LALT, LT(8, KC_ESC), LT(6, KC_TAB), LT( 5, KC_SPC) , LT( 10,KC_ENT), LT(9, KC_BSPC), LT(11,KC_DEL ), KC_RGUI
 ),
