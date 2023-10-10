5ac21c6dc00c28a0a2b2b4606e7d63f527c8b0d8
Niklas <NiklasAkita@users.noreply.github.com>
2023-10-10 08:57:00 +0200

    added new layers

--- keyboards/splitkb/aurora/lily58/keymaps/custom/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/custom/keymap.c
@@ -11,8 +11,11 @@ enum layer_number {
   _BUTTON,
   _MEDIA,
   _NUM,
+  _NUM2,
   _SYM,
-  _FUN
+  _FUN,
+  _SYM2,
+  _NAV_SINGLE
 };
 
 
@@ -35,9 +38,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_QWERTY] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
-  KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, LT(8, KC_ESC), LT(6, KC_TAB), LT( 5, KC_SPC) , LT( 10,KC_ENT), LT(9, KC_BSPC), LT(11,KC_DEL ), KC_RGUI
+  KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                        KC_RGUI, LT(8, KC_ESC), LT(6, KC_TAB), LT( 5, KC_SPC) , LT( 13,KC_ENT), LT(10, KC_BSPC), LT(12,KC_DEL ), KC_RGUI
 ),
 
 /* QWERTY
@@ -57,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_COLEMAK_DH] = LAYOUT(
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_GRV,
-  KC_TAB,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,    KC_MINS,
-  KC_LCTL,  KC_A , LALT_T(KC_R), CTL_T(KC_S), SFT_T(KC_T), KC_G,         KC_M,    RSFT_T(KC_N),    RCTL_T(KC_E),   RALT_T(KC_I),    KC_O, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_LBRC,  KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, LT(8, KC_ESC), LT(6, KC_TAB), LT( 5, KC_SPC) , LT( 10,KC_ENT), LT(9, KC_BSPC), LT(11,KC_DEL ), KC_RGUI
+  KC_LALT,   KC_Q,   KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT,    KC_RALT,
+  KC_LSFT,  KC_A , LALT_T(KC_R), CTL_T(KC_S), SFT_T(KC_T), KC_G,         KC_M,    RSFT_T(KC_N),    RCTL_T(KC_E),   RALT_T(KC_I),    KC_O, KC_QUOT KC_RSFT,
+  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_D,    KC_V, KC_LBRC,  KC_RBRC,  KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RCTL,
+                        KC_RGUI, LT(8, KC_ESC), LT(6, KC_TAB), LT( 5, KC_SPC) , LT( 13,KC_ENT), LT(10, KC_BSPC), LT(12,KC_DEL ), KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -129,8 +132,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NAV] = LAYOUT(
   _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______,                      KC_AGIN,  KC_PSTE, KC_COPY, KC_CUT, KC_UNDO, _______,
-  _______, _______, KC_LALT, KC_LCTL, KC_LSFT,   _______,                    KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
+  _______, _______, _______, _______, _______, _______,                      KC_AGIN,  KC_PSTE, KC_UP, KC_CUT, KC_UNDO, _______,
+  _______, _______, KC_LALT, KC_LCTL, KC_LSFT,   _______,                    KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, _______,
   _______, _______, _______, _______, _______, _______,   _______, _______,  KC_INS, KC_HOME, KC_PGDN,  KC_PGUP, KC_END,  _______,
                              _______, _______, _______,  _______, KC_ENTER,  KC_BSPC, KC_DELETE, _______
   ),
@@ -166,6 +169,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_GRAVE,   KC_1,    KC_2,    KC_3, KC_BSLS,   _______, _______,  _______,  _______, _______, _______, _______,  _______,
                              _______,  KC_DOT,    KC_0,  KC_MINS,  _______, _______, _______, _______
   ),
+   [_NUM2] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  _______, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                     _______, _______, _______, _______, _______, _______,
+  _______, KC_SCLN,    KC_4,    KC_5,    KC_6, KC_EQL,                    _______, KC_RSFT,KC_RCTL,KC_RALT, _______, _______,
+  _______, KC_GRAVE,   KC_1,    KC_2,    KC_3, KC_BSLS,   _______, _______,  _______,  _______, _______, _______, _______,  _______,
+                             _______,  KC_DOT,    KC_0,  KC_MINS,  _______, _______, _______, _______
+  ),
 
 
   [_SYM] = LAYOUT(
@@ -182,14 +192,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_F11,  KC_F4, KC_F5, KC_F6, KC_SCRL,                    _______, KC_RSFT,KC_RCTL,KC_RALT, _______, _______,
   _______, KC_F10, KC_F1,   KC_F2, KC_F3, KC_PAUS,   _______, _______,  _______,  _______, _______, _______, _______,  _______,
                              _______, KC_APP, KC_SPC, KC_TAB,  _______, _______, _______, _______
-  )
+  ),
+
+  [_SYM2] = LAYOUT(
+  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
+  _______, KC_PERC, KC_AMPR, KC_LCBR, KC_RCBR, KC_ASTR,                     _______, _______, _______, _______, _______, _______,
+  _______, KC_COLN,  KC_DLR, KC_LPRN, KC_RPRN, KC_PLUS,                    _______, KC_RSFT,KC_RCTL,KC_RALT, _______, _______,
+  KC_HASH, KC_TILD, KC_EXLM,  KC_LBRC, KC_RBRC, KC_PIPE,   _______, _______,  _______,  _______, _______, _______, _______,  _______,
+                             _______,  KC_AT, KC_CIRC,  KC_UNDS,  _______, _______, _______, _______
+  ),
+
+  [_NAV_SINGLE] = LAYOUT(
+  _______, _______, _______, KC_UP, _______, _______,                     _______, _______, KC_UP, _______, _______, _______,
+  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,                      KC_AGIN,  KC_LEFT, KC_DOWN, KC_RGHT, KC_UNDO, _______,
+  _______, _______, KC_LALT, KC_LCTL, KC_LSFT,   _______,                    KC_CAPS, KC_RSFT, KC_RCTL, KC_RALT, XXXXXXX, _______,
+  _______, _______, _______, _______, _______, _______,   _______, _______,  KC_INS, KC_HOME, KC_PGDN,  KC_PGUP, KC_END,  _______,
+                             _______, _______, _______,  _______, KC_ENTER,  KC_BSPC, KC_DELETE, _______
+  ),
 };
 
 
 
+// layer_state_t layer_state_set_user(layer_state_t state) {
+//   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+// }
+
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+    switch (get_highest_layer(state)) {
+    case _RAISE:
+        rgblight_setrgb (0x00,  0x00, 0xFF);
+        break;
+    case _LOWER:
+        rgblight_setrgb (0xFF,  0x00, 0x00);
+        break;
+    case _ADJUST:
+        rgblight_setrgb (0x7A,  0x00, 0xFF);
+        break;
+    default: //  for any other layers, or the default layer
+        rgblight_setrgb (0x00,  0xFF, 0xFF);
+        break;
+    }
+  return state;
 }
+
 
 //SSD1306 OLED update loop, make sure to enable OLED_ENABLE=yes in rules.mk
 #ifdef OLED_ENABLE
