e6ec9f3db4609f26f85f50c3b08fa15a6ef8706b
Niklas <NiklasAkita@users.noreply.github.com>
2023-10-15 20:34:47 +0200

    added nmpad and tweaked gaming layer

--- keyboards/splitkb/aurora/lily58/keymaps/custom/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/custom/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LSFT,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                        KC_LALT, KC_ESC, KC_TAB, KC_SPC , KC_ENT, KC_BSPC, LT(12,KC_DEL ), KC_RGUI
+                        KC_LCTL, KC_LALT,KC_SPC  , KC_TAB, KC_ENT, KC_BSPC, LT(12,KC_DEL ), KC_RGUI
 ),
 
 /* QWERTY
@@ -169,18 +169,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NUM] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  _______, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                     _______, _______, _______, _______, _______, _______,
-  _______, KC_SCLN,    KC_4,    KC_5,    KC_6, KC_EQL,                    _______, KC_RSFT,KC_RCTL,KC_RALT, _______, _______,
-  _______, KC_GRAVE,   KC_1,    KC_2,    KC_3, KC_BSLS,   _______, _______,  _______,  _______, _______, _______, _______,  _______,
-                             _______,  KC_DOT,    KC_0,  KC_MINS,  _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______,                     _______, _______, KC_NUBS, KC_PAST, KC_PEQL, _______,
+  _______, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                     _______, KC_P7, KC_P8, KC_P9, KC_PMNS, _______,
+  _______, KC_SCLN,    KC_4,    KC_5,    KC_6, KC_EQL,                    _______, KC_P4,KC_P5,KC_P6, KC_PPLS, _______,
+  _______, KC_GRAVE,   KC_1,    KC_2,    KC_3, KC_BSLS,   _______, _______,  _______,  KC_P1, KC_P2, KC_P3, KC_PENT,  _______,
+                             _______,  KC_DOT,    KC_0,  KC_MINS,  _______, _______, KC_P0, KC_PDOT
   ),
    [_NUM2] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  _______, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                     _______, _______, _______, _______, _______, _______,
-  _______, KC_SCLN,    KC_4,    KC_5,    KC_6, KC_EQL,                    _______, KC_RSFT,KC_RCTL,KC_RALT, _______, _______,
-  _______, KC_GRAVE,   KC_1,    KC_2,    KC_3, KC_BSLS,   _______, _______,  _______,  _______, _______, _______, _______,  _______,
-                             _______,  KC_DOT,    KC_0,  KC_MINS,  _______, _______, _______, _______
+  _______, _______, _______, _______, _______, _______,                     _______, _______, KC_NUBS, KC_PAST, KC_PEQL, _______,
+  _______, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                     _______, KC_P7, KC_P8, KC_P9, KC_PMNS, _______,
+  _______, KC_SCLN,    KC_4,    KC_5,    KC_6, KC_EQL,                    _______, KC_P4,KC_P5,KC_P6, KC_PPLS, _______,
+  _______, KC_GRAVE,   KC_1,    KC_2,    KC_3, KC_BSLS,   _______, _______,  _______,  KC_P1, KC_P2, KC_P3, KC_PENT,  _______,
+                             _______,  KC_DOT,    KC_0,  KC_MINS,  _______, _______, KC_P0, KC_PDOT
   ),
 
 
